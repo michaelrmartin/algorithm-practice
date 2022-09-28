2017ee40b14e7066062cb685f5b3a2fba34821c4
michaelrmartin <iammichaelmartin@gmail.com>
2022-09-28 16:45:51 -0500

    Adds solution to array_max.rb

---
+++ arrray_max.rb
@@ -0,0 +1,36 @@
+# Write a function that returns the greatest value from an array of numbers.
+
+# Input: [5, 17, -4, 20, 12]
+# Output: 20
+
+# Clarify
+#   Input: [6, 8, 234, 235, 0, -4]
+#   Output: 235
+# 
+
+# Visualize
+# variable to hold highest value number
+# index variable to increment through array 
+# return variable at end of function
+#                 V
+# [5, 17, -4, 20, 12]
+# max_number = 20
+
+# Code
+
+def max_number(array)
+  index = 0
+  max = 0
+  while index < array.length
+    selected = array[index]
+    if selected > max
+      max = selected
+    end
+    index += 1
+  end
+
+  return max
+  
+end
+
+p max_number([6, 8, 234, 235, 0, -4])
