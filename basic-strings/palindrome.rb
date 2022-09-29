4e0406a3dc12b7c920d6f0d4a763959a162637d7
michaelrmartin <iammichaelmartin@gmail.com>
2022-09-29 14:08:06 -0500

    Adds hamming.rb and palindrome.rb problems and palindrome solution

---
+++ basic-strings/palindrome.rb
@@ -0,0 +1,16 @@
+# Palindrome
+# Description
+# Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+
+# Input: “racecar”
+# Output: true
+
+# Input: “baloney”
+# Output: false
+
+def palindrome(string)
+  reverse = ""
+  index = string.length - 1
+  
+
+end
