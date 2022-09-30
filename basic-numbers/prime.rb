e8f2be8875c5797d3aba83c6ea9d6575ed7d1b5b
michaelrmartin <iammichaelmartin@gmail.com>
2022-09-29 19:13:50 -0500

    Updates solution to prime.rb

--- basic-numbers/prime.rb
+++ basic-numbers/prime.rb
@@ -3,17 +3,21 @@
 # Description
 # Write a function that returns whether a given number is a prime number.
 
-def prime(integer)
+def is_prime?(integer)
   index = 2
   while index < integer
+
     if integer % index == 0
       return false
-      break
-    else
-      index += 1
     end
-    return true
+    
+    index += 1
+    
   end
+
+  return true
+
 end
 
-p prime(3)
+
+p is_prime?(3)
