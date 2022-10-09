50c669ddbbc8980a26e1efadeaa98223f2ad3ce1
michaelrmartin <iammichaelmartin@gmail.com>
2022-10-09 12:44:14 -0500

    Adds problems for basic-numbers and two-pointers

---
+++ two-pointers/merge_sorted_arrays.rb
@@ -0,0 +1,10 @@
+# Merge Sorted Arrays ->
+
+# Description
+# Given two sorted arrays, merge the second array into the first array while ensuring that the first array remains sorted. Do not use any built-in sort methods.
+
+# Input :
+# A : [1, 5, 8]
+# B : [6, 9]
+
+# Modified A : [1, 5, 6, 8, 9]
