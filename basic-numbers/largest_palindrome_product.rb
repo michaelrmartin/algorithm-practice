e2e34d5dbdb9d533e3eb46a4cf0c8e6aa7149711
michaelrmartin <iammichaelmartin@gmail.com>
2022-10-11 14:43:26 -0500

    Adds solution to array_mesh_1.rb

--- basic-numbers/largest_palindrome_product.rb
+++ basic-numbers/largest_palindrome_product.rb
@@ -4,3 +4,41 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+# Visualize
+# create a method called largest_palindrome that takes a number as an argument?
+# the number will be equal to the number of digits for the two numbers, indicating max number of digits for each number (so max is string concat 9 + 9 + 9?) than convert to integer?
+# need max_palindrome variable to store largest answer
+# need product to store response
+# two index variables, one starts at 999, two starts at 999, stop when palindrome max_number == palindrome 
+# while loop (2 while loops? for now, refactor?)
+# if conditional to check if palindrome, convert response to string and check if string[0] = string[-1]
+
+
+def largest_palindrome_product(number)
+  max_palindrome = nil
+  index1 = (10 ** number)
+  index2 = index1 - 1 
+
+  while !max_palindrome
+    index -= 1
+    while index2 > ((10 ** (number - 1)) -1)
+      product = index1 * index2
+      p product
+      string = product.to_s
+      if string == string.reverse
+        max_palindrome = string.to_i
+        p max_palindrome
+        break
+      else
+        index2 -= 1
+      end
+    end
+    index2 = index1
+  end
+  return max_palindrome
+end
+
+p largest_palindrome_product(3)
+      
+    
--- two-pointers/array_mesh_1.rb
+++ two-pointers/array_mesh_1.rb
@@ -5,3 +5,24 @@
 
 # Input: ["a", "b", "c"], ["d", "e", "f", "g"]
 # Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+def mesh_arrays(array1, array2)
+  index1 = 0
+  index2 = 0
+  meshed = []
+
+  while index1 < array1.length
+    while index2 < array2.length
+      meshed << array1[index1] + array2[index2]
+      index2 += 1
+    end
+    index2 = 0
+    index1 += 1
+  end
+
+  return meshed
+
+end
+
+p mesh_arrays(["a", "b", "c"], ["d", "e", "f", "g"])
+      
