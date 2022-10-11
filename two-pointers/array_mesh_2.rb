458dee013a54c3be2b9473159d5df72513e9bd04
michaelrmartin <iammichaelmartin@gmail.com>
2022-10-11 14:52:20 -0500

    Adds solution to array_mesh_2.rb

--- two-pointers/array_mesh_2.rb
+++ two-pointers/array_mesh_2.rb
@@ -5,3 +5,26 @@
 
 # Input: ["a", "b", "c", "d"]
 # Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+def mesh_array(array)
+  index1 = 0
+  index2 = 0
+  meshed = []
+
+  while index1 < array.length
+    while index2 < array.length
+      if array[index1] != array[index2]
+        meshed << array[index1] + array[index2]
+        index2 += 1
+      else
+        index2 += 1
+      end
+    end
+    index2 = 0
+    index1 += 1
+  end
+
+  return meshed
+end
+
+p mesh_array(["a", "b", "c", "d"])
