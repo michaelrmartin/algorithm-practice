b3280fde1f95c1635b54dd058cb0b44dc6c3bdf7
michaelrmartin <iammichaelmartin@gmail.com>
2022-10-11 16:31:44 -0500

    Adds solution to merge_sorted_arrays.rb

--- two-pointers/merge_sorted_arrays.rb
+++ two-pointers/merge_sorted_arrays.rb
@@ -8,3 +8,38 @@
 # B : [6, 9]
 
 # Modified A : [1, 5, 6, 8, 9]
+
+def merge(nums1, nums2)
+
+  index1 = 0
+  index2 = 0
+  merged = []
+
+  while index1 < nums1.length || index2 < nums2.length
+    if !nums1[index1]
+      merged << nums2[index2]
+      index2 += 1
+      break
+    end
+
+    if !nums2[index2]
+      merged << nums1[index1]
+      index1 += 1
+      break
+    end
+    
+    if nums1[index1] < nums2[index2]
+      merged << nums1[index1]
+      index1 += 1
+    else
+      merged << nums2[index2]
+      index2 += 1
+    end
+  end
+
+  return merged
+
+end
+
+p merge([1, 5, 8], [6, 9])
+
